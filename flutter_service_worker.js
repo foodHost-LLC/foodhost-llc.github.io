26ad8c0333a1428750678969025881a967369ee8
Okonkwo Clovis Chisom <Saintiano@gmail.com>
2023-10-18 08:54:46 +0100

    Update
    Fixed side bar menu bug

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -70,9 +70,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ed4e9f46a793f4bb46abaccb8517dad9",
-"/": "ed4e9f46a793f4bb46abaccb8517dad9",
-"main.dart.js": "18d831b5a5f1b7f10a0b965e1e2a5f2c",
+"index.html": "e0270a22a02c399952d99ceb7e577ae6",
+"/": "e0270a22a02c399952d99ceb7e577ae6",
+"main.dart.js": "9019882c5b1c0675ff9b68833c469c94",
 "manifest.json": "f69eaf2720b771a2df07842e20795d2a",
 "version.json": "f0c0ce745bb6d20dc456a77994d94341"
 };
